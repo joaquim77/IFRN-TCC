f9e845d92d2a36646b65746fd455bf80712687b5
Fábio Penha <fpenha@gmail.com>
2015-06-17 11:21:45 -0300

    Adicionado o método principal.

--- DiaSoft/src/teste.java
+++ DiaSoft/src/teste.java
@@ -1,4 +1,9 @@
 
+/**
+ * TODO Joca, altere o nome do arquivo para Teste 
+ */
 public class teste {
-
+     public static void main(String[] args){
+         System.out.println("Ola GitHUB");
+     }
 }
